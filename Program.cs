cc23af3e01b45e2b8a7ce9ee319fab0a034c611b
IBugagashI <popykin.alek@yandex.ru>
2022-12-31 00:34:18 +0300

    End of the solution

--- Program.cs
+++ Program.cs
@@ -1,24 +1,50 @@
-﻿void InputArray(string[] array)
+﻿void SelectedArray(string[] array1, string[] array2)
 {
-    for (int i = 0; i < array.Length; i++)
+    int index = 0;
+    for (int i = 0; i < array1.Length; i++)
     {
-        Console.Write("Введите значение: ");
-        array[i] = Console.ReadLine();
+        if(array1[i].Length <= 3)
+        array2[index++] = array1[i];
     }
 }
 
-void ReliseArray(string[] array)
+void ReliseArray(string[] array1)
 {
     Console.Write("Ваш массив: ");
-    for (int i = 0; i < array.Length; i++)
-        {
-            Console.Write($"{array[i]} ");
-        }
+    for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write($"{array1[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void ReliseSelectedArray(string[] array2)
+{
+    Console.Write("Перебраный массив массив: ");
+    for (int i = 0; i < array2.Length; i++)
+    {
+        Console.Write($"{array2[i]} ");
+    }
+    Console.WriteLine();
 }
 
 Console.Clear();
 Console.Write("Укажите размер массива: ");
 int size = Convert.ToInt32(Console.ReadLine());
-string[] array = new string[size];
-InputArray(array);
-ReliseArray(array);
+int count = 0;
+string[] array1 = new string[size];
+for (int i = 0; i < array1.Length; i++)
+    {
+        Console.Write("Введите значение: ");
+        array1[i] = Console.ReadLine();
+        if(array1[i].Length <= 3) 
+        {
+            count++;
+        }
+    }
+
+string[] array2 = new string[count];
+
+ReliseArray(array1);
+SelectedArray(array1, array2);
+ReliseSelectedArray(array2);
