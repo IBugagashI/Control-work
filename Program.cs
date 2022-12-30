4e911d1568475502b06d5368bc196b265cdc825c
IBugagashI <popykin.alek@yandex.ru>
2022-12-30 19:36:51 +0300

    Input Array

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿void InputArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write("Введите значение: ");
+        array[i] = Console.ReadLine();
+    }
+}
+
+Console.Clear();
+//Console.Write("Впишите элементы массива через пробел:");
+//string[] meaning = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+string[] array = new string[3];
+InputArray(array);
+Console.Write(array);
